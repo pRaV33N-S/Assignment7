7188a5513788fcc15a9dd6fc620e3009065e2a96
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-14 14:45:59 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignMent7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add("Apple");
+            list.Add("Banana");
+            list.Add("Cherry");
+            list.Add("Date");
+            list.Add("ElderBerry");
+            Console.WriteLine("The total number of elements "+list.Count);
+            Console.WriteLine("Does the element \"Date\" contains in the list?\n True \t False");
+            Console.WriteLine("\t"+list.Contains("Date"));
+            list.Insert(2, "Fig");
+            list.Remove("Banana");
+            foreach (string item in list)
+                Console.WriteLine(item);
+        }
+    }
+}
